6dcf0839d51179d1d82a317d5e6b2d6868dc915d
YaSanyaBeats <alexandr191261@yandex.ru>
2024-08-01 11:41:43 +0700

    Main Page

--- src/js/app.js
+++ src/js/app.js
@@ -21,26 +21,99 @@ function isWebp() {
 
 isWebp();
 
-function initCopyCode() {
-    let codes = document.querySelectorAll('.code__body');
-    codes.forEach((elem) => {
-        elem.addEventListener('click', (event) => {
-            UIkit.notification.closeAll()
-            navigator.clipboard.writeText(elem.innerText).then(function() {
-                UIkit.notification({
-                    message: 'Код успешно скопирован',
-                    status: 'primary',
-                    pos: 'bottom-center',
-                    group: false,
-                    timeout: 5000
-                });
-            }, function(err) {
-                console.error('Произошла ошибка при копировании текста: ', err);
-            });
-        })
-    })
+function setGradientBullets(swiper, paginationEl) {
+    // Накладываем градиент на точки пагинации
+    let bullets = [...paginationEl.querySelectorAll('.swiper-pagination-bullet')];
+    let activeBullet = bullets.findIndex(bullet => {
+        return bullet.classList.contains('swiper-pagination-bullet-active');
+    });
+
+    const opacityMaxLevel = 5;
+    const maxOpacity = 80;
+    const stepOpacity = maxOpacity / (opacityMaxLevel - 1);
+
+    bullets.forEach((bullet, index) => {
+        let deltaOpacity = Math.abs(index - activeBullet);
+        let opacity = maxOpacity - stepOpacity * deltaOpacity;
+        if(opacity <= 0) {
+            opacity = 10;
+        }
+        bullet.style.opacity = '.' + opacity;
+    });
+}
+
+function initSliders() {
+    const heroSwiper = new Swiper('.hero__swiper', {
+        loop: true,
+        parallax: true,
+        allowTouchMove: false,
+        speed: 900,
+        autoplay: {
+            delay: 7000,
+        },
+        pagination: {
+            el: '.hero__swiper-pagination',
+            clickable: true,
+        },
+        on: {
+            paginationUpdate: setGradientBullets
+        },
+    });
+
+    const productsSwiper = new Swiper('.products-slider__swiper', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 5,
+        
+        navigation: {
+            prevEl: '.products-slider__nav-button_prev',
+            nextEl: '.products-slider__nav-button_next'
+        },
+    });
+
+    const categoriesSwiper = new Swiper('.categories-slider__swiper', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 5,
+        
+        navigation: {
+            prevEl: '.categories-slider__nav-button_prev',
+            nextEl: '.categories-slider__nav-button_next'
+        },
+    });
+
+    const aboutSwiper = new Swiper('.about__swiper', {
+        slidesPerView:'auto',
+        spaceBetween: 5,
+        
+        pagination: {
+            el: '.about__swiper-pagination',
+            clickable: true,
+        },
+        on: {
+            paginationUpdate: setGradientBullets
+        },
+
+        navigation: {
+            prevEl: '.about__nav-button_prev',
+            nextEl: '.about__nav-button_next'
+        },
+    });
+
+    const whereSwiper = new Swiper('.where__swiper', {
+        slidesPerView:'auto',
+        centeredSlides: true,
+        loop: true,
+        loopAddBlankSlides: true,
+        parallax: true,
+
+        navigation: {
+            prevEl: '.where__nav-button_prev',
+            nextEl: '.where__nav-button_next'
+        },
+    });
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    initCopyCode();
+    initSliders();
 })
